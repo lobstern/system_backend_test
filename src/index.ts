a4ce23e93c76d4828e5ccb6385c70216d4d65256
Good Bob <shtyrovvs@gmail.com>
2023-06-22 02:59:48 +0800

    New port 443

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express, {Request, Response} from 'express'
 const app = express()
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 443
 
 app.get('/', (req: Request, res: Response) => {
     let helloM = "Hello Bob"
