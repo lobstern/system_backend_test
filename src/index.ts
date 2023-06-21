73f37542a3af1466d7cb18cff7f0644def8ebd59
Good Bob <shtyrovvs@gmail.com>
2023-06-22 02:26:35 +0800

    Changed port into 80

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express, {Request, Response} from 'express'
 const app = express()
-const port = 3000
+const port = process.env.PORT || 8080
 
 app.get('/', (req: Request, res: Response) => {
     let helloM = "Hello Bob"
