6d2250789937ba2570c40b2c06ff183eb202ed01
Good Bob <shtyrovvs@gmail.com>
2023-06-22 19:42:56 +0800

    Playing with get an query

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,29 @@ import express, {Request, Response} from 'express'
 const app = express()
 const port = process.env.PORT || 80
 
+const classes =[{id: 1, title: 'uniqueland'}, {id: 2, title: 'starland'}]
+
 app.get('/', (req: Request, res: Response) => {
     let helloM = "Hello Bobius"
   res.send(helloM)
+})
+
+app.get('/classes', (req: Request, res: Response) => {
+  if (req.query.title) {
+    let searchString = req.query.title.toString()
+    res.send(classes.filter(c => c.title.indexOf(searchString) > -1))
+  } else {
+    res.send(classes)
+  }
+})
+
+app.get('/classes/:id', (req: Request, res: Response) => {
+  let winlandClassId = classes.find(c => c.id === +req.params.id)
+  if (winlandClassId) {
+    res.send(winlandClassId)
+  } else {
+    res.send(404)
+  }
 })
 
 app.listen(port, () => {
